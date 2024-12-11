49ab9dcba8746c959b05ee972ac443cfe48ab3a9
Beliypozhizni <beliypozhizni@gmail.com>
2024-12-11 10:52:35 +0300

    The task is completed

---
+++ src/Elevator.java
@@ -0,0 +1,41 @@
+public class Elevator {
+    int minFloor;
+    int maxFloor;
+    int currentFloor;
+
+    public Elevator(int minFloor, int maxFloor) {
+        this.minFloor = minFloor;
+        this.maxFloor = maxFloor;
+        this.currentFloor = 1;
+    }
+
+    public int getCurrentFloor() {
+        return currentFloor;
+    }
+
+    public void moveDown() {
+        currentFloor--;
+    }
+
+    public void moveUp() {
+        currentFloor++;
+    }
+
+    public void move(int floor) {
+        if (floor == currentFloor) {
+            return;
+        }
+        if (floor >= minFloor && floor <= maxFloor) {
+            while (floor < currentFloor || floor > currentFloor) {
+                if (floor < currentFloor) {
+                    moveDown();
+                } else {
+                    moveUp();
+                }
+                System.out.println("Текущий этаж: " + currentFloor);
+            }
+        } else {
+            System.out.println("Указан несуществующий этаж");
+        }
+    }
+}
